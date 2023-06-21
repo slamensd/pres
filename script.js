d31085a740be4047b09b59ce19bb4218bda4fe49
slamensd <58567346+slamensd@users.noreply.github.com>
2023-06-21 22:09:57 +0000

    update

--- script.js
+++ script.js
@@ -4,107 +4,85 @@ const tableName = 'Slides';
 const apiKey = 'keyzbt7lLQxpiP1MO';
 const headers = { Authorization: `Bearer ${apiKey}`, 'Content-Type': 'application/json' };
 
-let slides = Array.from({ length: 13 }, (_, i) => ({
-    src: i === 0 ? 'slides/Slide0.png' : `slides/Slide${i}.png`,
-    caption: `Slide ${i}`,
-    status: "unlocked"
-}));
-
+let slides = [
+  {
+    caption: '',
+    status: 'unlocked',
+    content: `
+      <h2 class="headline">Explore the Frensville Ecosystem</h2>
+      <input type="text" name="twitter" id="twitter-input" placeholder="What is your Twitter handle?" required>
+      <button type="submit" data-slide="0" class="cta-button">Get Started</button>
+    `,
+  },
+];
 
 let twitterAccount = '';
 
 const createForm = (slideIndex) => {
-    const form = document.createElement('form');
-    form.innerHTML = `
-        ${slideIndex === 0 ? '<input type="text" name="twitter" id="twitter-input" placeholder="Your Twitter Account" required>' : '<textarea name="question" placeholder="Your Question"></textarea>'}
-        <div class="buttons">
-            ${slideIndex > 0 ? '<button type="button" class="prev" data-slide="'+slideIndex+'">Previous</button>' : ''}
-            ${slideIndex === 0 ? '' : '<button type="button" class="skip" data-slide="${slideIndex}">Skip</button>'}
-            <button type="submit" data-slide="${slideIndex}">${slideIndex === 0 ? 'Get Started' : 'Submit'}</button>
-        </div>
-    `;
-    form.onsubmit = async (event) => {
-        event.preventDefault();
-        const { twitter, question } = event.target.elements;
-        if (twitter) {
-            twitterAccount = twitter.value;
-        }
-        if (twitterAccount && question) {
-            await submitQuestion(slideIndex, twitterAccount, question.value);
-            event.target.reset();
-        }
-        showNextSlide(slideIndex);
-    };
-    const skipButton = form.querySelector('.skip');
-    if (skipButton) {
-        skipButton.onclick = () => showNextSlide(slideIndex);
+  const form = document.createElement('form');
+  form.innerHTML = slides[slideIndex].content;
+  form.onsubmit = async (event) => {
+    event.preventDefault();
+    const { twitter } = event.target.elements;
+    if (twitter) {
+      twitterAccount = twitter.value;
     }
-    const prevButton = form.querySelector('.prev');
-    if (prevButton) {
-        prevButton.onclick = () => showPreviousSlide(slideIndex);
+    if (twitterAccount) {
+      await submitQuestion(slideIndex, twitterAccount);
+      event.target.reset();
     }
-    return form;
+    showNextSlide(slideIndex);
+  };
+  return form;
 };
 
 const createSlide = (slideData, index) => {
-    const slide = document.createElement('div');
-    slide.className = 'slide-container';
-    slide.innerHTML = `
-        ${index !== 0 ? '<button class="prev" data-slide="'+index+'">Previous</button>' : ''}
-        <div class="slide">
-            ${index !== 0 ? '<img src="'+slideData.src+'" alt="Slide">' : ''}
-            <p>${slideData.caption}</p>
-        </div>
-    `;
-    slide.appendChild(createForm(index));
-    return slide;
+  const slide = document.createElement('div');
+  slide.className = 'slide-container';
+  slide.innerHTML = `
+    <div class="slide">
+      ${slideData.content}
+    </div>
+  `;
+  slide.appendChild(createForm(index));
+  return slide;
 };
 
-const submitQuestion = async (slideIndex, twitter, question) => {
-    const data = {
-        "records": [
-            {
-                "fields": {
-                    "Slide": `Slide ${slideIndex}`,
-                    "Twitter": twitter,
-                    "Question": question
-                }
-            }
-        ]
-    };
-    await fetch(`https://api.airtable.com/v0/${baseId}/${tableName}`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data)
-    });
+const submitQuestion = async (slideIndex, twitter) => {
+  const data = {
+    records: [
+      {
+        fields: {
+          Slide: `Slide ${slideIndex}`,
+          Twitter: twitter,
+        },
+      },
+    ],
+  };
+  await fetch(`https://api.airtable.com/v0/${baseId}/${tableName}`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
+  });
 };
 
 const showNextSlide = (index) => {
-    const currentSlide = document.querySelectorAll('.slide-container')[index];
-    currentSlide.style.display = 'none';
-    const nextSlide = document.querySelectorAll('.slide-container')[index + 1];
-    if (nextSlide) {
-        nextSlide.style.display = 'flex';
-    }
-};
-
-const showPreviousSlide = (index) => {
-    const currentSlide = document.querySelectorAll('.slide-container')[index];
-    currentSlide.style.display = 'none';
-    const previousSlide = document.querySelectorAll('.slide-container')[index - 1];
-    if (previousSlide) {
-        previousSlide.style.display = 'flex';
-    }
+  const currentSlide = document.querySelectorAll('.slide-container')[index];
+  currentSlide.style.display = 'none';
+  const nextSlide = document.querySelectorAll('.slide-container')[index + 1];
+  if (nextSlide) {
+    nextSlide.style.display = 'flex';
+  }
 };
 
 const updateSlides = () => {
-    const container = document.getElementById('slides-container');
-    container.innerHTML = '';
-    slides.forEach((slideData, index) => {
-        const slide = createSlide(slideData, index);
-        slide.style.display = index === 0 ? 'flex' : 'none';
-        container.appendChild(slide);
-    });
+  const container = document.getElementById('slides-container');
+  container.innerHTML = '';
+  slides.forEach((slideData, index) => {
+    const slide = createSlide(slideData, index);
+    slide.style.display = index === 0 ? 'flex' : 'none';
+    container.appendChild(slide);
+  });
 };
 
 updateSlides();
