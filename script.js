0938986df7eefa4f2a8ff9a57cc5aa46365c60b1
slamensd <58567346+slamensd@users.noreply.github.com>
2023-06-21 22:18:29 +0000

    okk

--- script.js
+++ script.js
@@ -8,13 +8,7 @@ let slides = [
   {
     caption: '',
     status: 'unlocked',
-    content: `
-      <div class="slide-content">
-        <h2 class="headline">Explore the Frensville Ecosystem</h2>
-        <input type="text" name="twitter" id="twitter-input" placeholder="What is your Twitter handle?" required>
-        <button type="submit" data-slide="0" class="cta-button">Get Started</button>
-      </div>
-    `,
+    content: '',
   },
 ];
 
