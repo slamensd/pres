c3c36fd31a609fdfbd7ea499e184621028476150
slamensd <58567346+slamensd@users.noreply.github.com>
2023-06-22 00:00:01 +0000

    okkk

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ const fetchQuestions = async () => {
 };
 
 const updateSlides = () => {
-  const container = document.getElementById('slides-container');
+  const container = document.getElementById('slide-container');
   container.innerHTML = '';
   slides.forEach((slideData, index) => {
     const slide = createSlide(slideData, index);
